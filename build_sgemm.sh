cb672a10a6ab2af319d1c37aeae514271490296a
Martin Krastev <blu.dark@gmail.com>
2020-08-22 11:35:47 +0300

    * move aarch64 uarch recognition to its own script * refactor build script

--- build_sgemm.sh
+++ build_sgemm.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-CC=${CXX:-clang++}
-CFLAGS=(
+CXX=${CXX:-clang++}
+CXXFLAGS=(
 	-std=c++11
 	-O3
 	-fstrict-aliasing
 	-fno-rtti
 	-fno-exceptions
 )
-CC_FILENAME=${CC##*/}
-if [[ $CC_FILENAME == *clang++* ]]; then
-	CFLAGS+=(
+CXX_FILENAME=${CXX##*/}
+if [[ $CXX_FILENAME == *clang++* ]]; then
+	CXXFLAGS+=(
 		-ffp-contract=fast
 	)
 
-elif [[ $CC_FILENAME == *g++* ]]; then
-	CFLAGS+=(
+elif [[ $CXX_FILENAME == *g++* ]]; then
+	CXXFLAGS+=(
 		-ffast-math
 	)
 
-elif [[ $CC_FILENAME == *icpc* ]]; then
-	CFLAGS+=(
+elif [[ $CXX_FILENAME == *icpc* ]]; then
+	CXXFLAGS+=(
 		-fp-model fast=2
 		-opt-prefetch=0
 		-opt-streaming-cache-evict=0
@@ -40,91 +40,19 @@ else
 	echo Unknown platform
 	exit 255
 fi
+
+source cxx_util.sh
+
 # use the machine name to detect armv8 devices with aarch64 kernels and armv7 userspaces
 UNAME_MACHINE=`uname -m`
 
 if [[ $UNAME_MACHINE == "aarch64" ]]; then
 
-	CFLAGS+=(
-		-DCACHELINE_SIZE=64
-		-march=armv8-a
-	)
-
-	# clang can fail auto-detecting the host armv8 cpu on some setups; collect all part numbers
-	IMPLR=`cat /proc/cpuinfo | grep "^CPU implementer" | sed s/^[^[:digit:]]*//`
-	UARCH=`cat /proc/cpuinfo | grep "^CPU part" | sed s/^[^[:digit:]]*//`
-
-	if   [ `echo $IMPLR | grep -c 0x41` -ne 0 ]; then # Arm Holdings
-		# in order of preference, in case of big.LITTLE
-		if   [ `echo $UARCH | grep -c 0xd0c` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a76 # cortex-n1
-			)
-		elif [ `echo $UARCH | grep -c 0xd0b` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a76
-			)
-		elif [ `echo $UARCH | grep -c 0xd0a` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a75
-			)
-		elif [ `echo $UARCH | grep -c 0xd09` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a73
-			)
-		elif [ `echo $UARCH | grep -c 0xd08` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a72
-			)
-		elif [ `echo $UARCH | grep -c 0xd07` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a57
-			)
-		elif [ `echo $UARCH | grep -c 0xd05` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a55
-			)
-		elif [ `echo $UARCH | grep -c 0xd04` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a35
-			)
-		elif [ `echo $UARCH | grep -c 0xd03` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a53
-			)
-		fi
-	elif [ `echo $IMPLR | grep -c 0x51` -ne 0 ]; then # Qualcomm
-		# in order of preference, in case of big.LITTLE
-		if   [ `echo $UARCH | grep -c 0x804` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a76 # kryo 4xx gold
-			)
-		elif [ `echo $UARCH | grep -c 0x805` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a55 # kryo 4xx silver
-			)
-		elif [ `echo $UARCH | grep -c 0x802` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a75 # kryo 3xx gold
-			)
-		elif [ `echo $UARCH | grep -c 0x803` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a55 # kryo 3xx silver
-			)
-		elif [ `echo $UARCH | grep -c 0x800` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a73 # kryo 2xx gold
-			)
-		elif [ `echo $UARCH | grep -c 0x801` -ne 0 ]; then
-			CFLAGS+=(
-				-mtune=cortex-a53 # kryo 2xx silver
-			)
-		fi
-	fi
+	cxx_uarch_arm
 
 elif [[ $HOSTTYPE == "i686" ]]; then
 
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-DCACHELINE_SIZE=32
 		-march=native
 		-mtune=native
@@ -132,7 +60,7 @@ elif [[ $HOSTTYPE == "i686" ]]; then
 
 elif [[ $HOSTTYPE == "x86_64" ]]; then
 
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-DCACHELINE_SIZE=64
 		-march=native
 		-mtune=native
@@ -140,7 +68,7 @@ elif [[ $HOSTTYPE == "x86_64" ]]; then
 
 elif [[ $HOSTTYPE == "powerpc64" || $HOSTTYPE == "ppc64" ]]; then
 
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-DCACHELINE_SIZE=64
 		-mpowerpc64
 		-mcpu=powerpc64
@@ -151,7 +79,7 @@ elif [[ $HOSTTYPE == "powerpc64" || $HOSTTYPE == "ppc64" ]]; then
 
 elif [[ $HOSTTYPE == "mipsel" ]]; then
 
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-DCACHELINE_SIZE=32
 		-march=mips32r5
 		-mtune=p5600
@@ -162,6 +90,6 @@ elif [[ $HOSTTYPE == "mipsel" ]]; then
 	)
 fi
 
-BUILD_CMD="-o sgemm "${CFLAGS[@]}" sgemm.cpp "${LFLAGS[@]}" "${@}
-echo $CC $BUILD_CMD
-"$CC" $BUILD_CMD
+BUILD_CMD="-o sgemm "${CXXFLAGS[@]}" sgemm.cpp "${LFLAGS[@]}" "${@}
+echo $CXX $BUILD_CMD
+$CXX $BUILD_CMD
---
+++ cxx_util.sh
@@ -0,0 +1,165 @@
+# update array CXXFLAGS with uarch-specific flags
+
+function cxx_uarch_arm() {
+	# clang can fail auto-detecting the host armv8 cpu on some setups; collect all part numbers
+	VENDOR=`cat /proc/cpuinfo | grep -m 1 "^CPU implementer" | sed s/^[^[:digit:]]*//`
+	UARCH=`cat /proc/cpuinfo | grep "^CPU part" | sed s/^[^[:digit:]]*//`
+
+	if   [[ $VENDOR == 0x41 ]]; then # Arm Holdings
+		# in order of preference, in case of big.LITTLE
+		if   [ `echo $UARCH | grep -c 0xd0c` -ne 0 ]; then # cortex-n1
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a76
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd0b` -ne 0 ]; then # cortex-a76
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a76
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd0a` -ne 0 ]; then # cortex-a75
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a75
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd09` -ne 0 ]; then # cortex-a73
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a73
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd08` -ne 0 ]; then # cortex-a72
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a72
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd07` -ne 0 ]; then # cortex-a57
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a57
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd05` -ne 0 ]; then # cortex-a55
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a55
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd04` -ne 0 ]; then # cortex-a35
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a35
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd03` -ne 0 ]; then # cortex-a53
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a53
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xd01` -ne 0 ]; then # cortex-a32
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mcpu=cortex-a32
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0xc0f` -ne 0 ]; then # cortex-a15
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a15
+				-DCACHELINE_SIZE=32
+			)
+		elif [ `echo $UARCH | grep -c 0xc0e` -ne 0 ]; then # cortex-a17
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a17
+				-DCACHELINE_SIZE=32
+			)
+		elif [ `echo $UARCH | grep -c 0xc0d` -ne 0 ]; then # cortex-a12
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a12
+				-DCACHELINE_SIZE=32
+			)
+		elif [ `echo $UARCH | grep -c 0xc09` -ne 0 ]; then # cortex-a9
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a9
+				-DCACHELINE_SIZE=32
+			)
+		elif [ `echo $UARCH | grep -c 0xc08` -ne 0 ]; then # cortex-a8
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a8
+				-DCACHELINE_SIZE=32
+			)
+		elif [ `echo $UARCH | grep -c 0xc07` -ne 0 ]; then # cortex-a7
+			CXXFLAGS+=(
+				-march=armv7-a
+				-mtune=cortex-a7
+				-DCACHELINE_SIZE=32
+			)
+		else
+			echo WARNING: unsupported uarch $UARCH by vendor $VENDOR
+			# set compiler flags to something sane
+			CXXFLAGS+=(
+				-DCACHELINE_SIZE=64
+			)
+		fi
+	elif [[ $VENDOR == 0x51 ]]; then # Qualcomm
+		# in order of preference, in case of big.LITTLE
+		if   [ `echo $UARCH | grep -c 0x804` -ne 0 ]; then # kryo 4xx gold
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a76
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0x805` -ne 0 ]; then # kryo 4xx silver
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a55
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0x802` -ne 0 ]; then # kryo 3xx gold
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a75
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0x803` -ne 0 ]; then # kryo 3xx silver
+			CXXFLAGS+=(
+				-march=armv8.2-a
+				-mtune=cortex-a55
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0x800` -ne 0 ]; then # kryo 2xx gold
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a73
+				-DCACHELINE_SIZE=64
+			)
+		elif [ `echo $UARCH | grep -c 0x801` -ne 0 ]; then # kryo 2xx silver
+			CXXFLAGS+=(
+				-march=armv8-a
+				-mtune=cortex-a53
+				-DCACHELINE_SIZE=64
+			)
+		else
+			echo WARNING: unsupported uarch $UARCH by vendor $VENDOR
+			# set compiler flags to something sane
+			CXXFLAGS+=(
+				-DCACHELINE_SIZE=64
+			)
+		fi
+	else
+		echo WARNING: unsupported uarch vendor $VENDOR
+		# set compiler flags to something sane
+		CXXFLAGS+=(
+			-DCACHELINE_SIZE=64
+		)
+	fi
+}
